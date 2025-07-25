a01fd7159bf7e89b382f3314c4490f5d4f7154c9
RichKingsasu <richkingsasu@gmail.com>
2025-07-25 03:55:14 +0000

    feat(pubsub-to-bq): HTTP server + event_utc column

--- pubsub-to-bq/main.py
+++ pubsub-to-bq/main.py
@@ -1,27 +1,55 @@
+
 import os
+
 from flask import Flask, request, abort
+
 from cloudevents.http import from_http
+
 from google.cloud import bigquery
 
+
+
 app = Flask(__name__)
+
 bq = bigquery.Client()
+
 TABLE = os.getenv("BQ_TABLE", "ttbot-466703.market_data.utbot_signals")
 
+
+
 @app.route("/", methods=["POST"])
+
 def receive_event():
+
     try:
+
         event = from_http(request.headers, request.get_data())
+
     except:
+
         abort(400, "invalid CloudEvent")
+
     data = event.data
+
     ts   = event.get("ce-time")
+
     row  = {"payload": data, "event_utc": ts}
+
     errors = bq.insert_rows_json(TABLE, [row])
+
     if errors:
+
         app.logger.error("BQ insert errors: %s", errors)
+
         abort(500, "BigQuery insert failed")
+
     return ("", 204)
 
+
+
 if __name__ == "__main__":
+
     port = int(os.environ.get("PORT", "8080"))
+
     app.run(host="0.0.0.0", port=port)
+
